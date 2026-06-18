aab2bd9405b1e4c2c40c80d1d367820327c99860
vijaygoswami896 <gurusacademy2021@gmail.com>
2026-06-18 21:59:06 +0530

    Initial commit

---
+++ src/test/java/com/vijay/gstBilling/GstBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijay.gstBilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GstBillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
